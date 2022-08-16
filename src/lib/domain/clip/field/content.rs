1cacbe3c398ee9e180fedc77a66efe619696ee08
Kyle Adkins <kyle.m.adkins@gmail.com>
2022-08-15 22:52:30 -0500

    Implement Content field

--- src/lib/domain/clip/field/content.rs
+++ src/lib/domain/clip/field/content.rs
@@ -1,4 +1,23 @@
+use crate::domain::clip::ClipError;
 use serde::{Serialize, Deserialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Content(String);
+
+impl Content {
+  pub fn new(content: &str) -> Result<Self, ClipError> {
+    if !content.trim().is_empty() {
+      Ok(Self(String::from(content)))
+    } else {
+      Err(ClipError::EmptyContent)
+    }
+  }
+
+  pub fn into_inner(self) -> String {
+    self.0
+  }
+
+  pub fn as_str(&self) -> &str {
+    self.0.as_str()
+  }
+}
