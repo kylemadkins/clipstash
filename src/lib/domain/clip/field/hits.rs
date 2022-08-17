267af9cb776bc8d9945a8415d906ae4ae9c4bcd2
Kyle Adkins <kyle.m.adkins@gmail.com>
2022-08-17 00:22:30 -0500

    Implement Hits field

--- src/lib/domain/clip/field/hits.rs
+++ src/lib/domain/clip/field/hits.rs
@@ -1,4 +1,21 @@
+use derive_more::Constructor;
 use serde::{Serialize, Deserialize};
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct Hits;
+#[derive(Clone, Constructor, Debug, Serialize, Deserialize)]
+pub struct Hits(u64);
+
+impl Hits {
+  /*
+
+  The `Constructor` trait from `derive_more` generates this code for us:
+
+  pub fn new(hits: u64) -> Self {
+    Self(hits)
+  }
+
+  */
+
+  pub fn into_inner(self) -> u64 {
+    self.0
+  }
+}
